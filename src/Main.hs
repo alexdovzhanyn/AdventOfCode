2fbbcef9ffed77526a9217297f28833feee8c47b
Alex <dovzhanyn.alex@gmail.com>
2024-07-24 19:20:56 -0400

    chore: tidy up the main output function so it is more data-driven

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,23 @@
 module Main (main) where
 
+import Control.Monad (forM_)
 import qualified Y2023.Day1.SolutionPart1 as D1P1
+
+solutions :: [[IO Int]]
+solutions = [ D1P1.getSolution ] : []
+
 
 main :: IO ()
 main = do
   putStrLn "---------------------------"
   putStrLn "    Advent of Code 2023"
   putStrLn "---------------------------"
-  d1p1Solution <- D1P1.getSolution
-  putStrLn ("Day 1, part 1 solution is: " ++ show d1p1Solution)
+
+  forM_ (zip [1..] solutions) $ outputSolutionParts
+
+
+outputSolutionParts :: (Int, [IO Int]) -> IO ()
+outputSolutionParts (dayNum, parts) = do
+  forM_ (zip ([1..] :: [Int]) parts) $ \(partNum, solution) -> do
+    sol <- solution
+    putStrLn ("Day " ++ show dayNum ++ ", part " ++ show partNum ++ " solution is: " ++ show sol)
